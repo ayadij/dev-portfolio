56b5a4b305f9427bf37b408853ec923b39a58f9a
ayadij <ayadijkwel@yahoo.com>
2018-01-22 13:58:39 -0700

    Integrated index action for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,41 @@
 class PortfoliosController < ApplicationController
+  before_action :set_blog,only: [:show, :edit, :update, :destroy]
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+  def show 
+  end
+
+
+  def edit
+  end
+
+
+  def create
+  end
+
+
+  def update
+  end
+
+
+  def destroy
+  end
+
+  
+
+
+
+  private
+
+  def set_blog
+  end
+
+
+  def blog_params
+  end
+
+
+
 end
