841f99daee447efc46283eb407175d5e74246104
ayadij <ayadijkwel@yahoo.com>
2018-01-25 14:20:49 -0700

    implemented concern for managing image data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124214027) do
+ActiveRecord::Schema.define(version: 20180125205422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,13 +18,13 @@ ActiveRecord::Schema.define(version: 20180124214027) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "slug"
-    t.integer "status",       default: 0
+    t.integer "status", default: 0
     t.bigint "topic_id"
-    t.index ["slug"],         name: "index_blogs_on_slug",      unique: true
-    t.index ["topic_id"],     name: "index_blogs_on_topic_id"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20180124214027) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
