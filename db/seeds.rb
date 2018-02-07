b2b969b9fb315c1aec1caa6a5037db5db12aa051
ayadij <ayadijkwel@yahoo.com>
2018-02-07 15:37:42 -0700

    updated file upload forms and refactored placeholder functionality into view

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@
     title: "Topic #{topic}"
   )
 end
-
 puts "3 Topics created"
+
 
 10.times do |blog|
   Blog.create!(
@@ -13,29 +13,8 @@ puts "3 Topics created"
     topic_id: Topic.last.id
   )
 end
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-3.times do |topic|
-  Topic.create!(
-    title: "Topic #{topic}")
-end
-puts "3 Topics created"
-
-
-10.times do |blog|
- Blog.create!( 
-  title: "My number #{blog} title",
-  body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
-  topic_id: Topic.last.id)
-end
-
 puts "10 blog posts created"
+
 
 5.times do |skill|
   Skill.create!(
@@ -43,8 +22,8 @@ puts "10 blog posts created"
     percent_utilized: 15
   )
 end
-
 puts "5 skills created"
+
 
 8.times do |portfolio_item|
   Portfolio.create!(
@@ -55,9 +34,7 @@ puts "5 skills created"
     thumb_image: "http://placehold.it/350x200"
   )
 end
-
-puts "9 portfolio items created"
-
+puts "8 portfolio items created"
 
 
 1.times do |portfolio_item|
@@ -69,15 +46,15 @@ puts "9 portfolio items created"
     thumb_image: "http://placehold.it/350x200"
     )
 end
-puts "9 portfolio items created"
+puts "1 portfolio items created"
+
 
 3.times do |technology|
   Portfolio.last.technologies.create!(
     name: "Technology #{technology}"
   )
 end
-
 puts "3 technologies created"
 
 
-
+User.create(name:"Dillon Porter", email:"dillon1@dillon.com", password:"asdfasdf", password_confirmation:"asdfasdf", role:"site_admin")
